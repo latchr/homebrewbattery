8ee1337849f2e439ac95239c06ccfc19835f863d
Lachlan J. Rogers <lachlan.j.rogers@quantum.diamonds>
2018-10-03 23:47:01 +1000

    Import mdates to make the figure part work.

--- pcm60x_logger.py
+++ pcm60x_logger.py
@@ -24,6 +24,7 @@ import datetime
 import numpy as np
 import gspread
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from oauth2client.service_account import ServiceAccountCredentials
 
 
