1730cb6c731c3a06832a9f17fd08f2bff1d3ff21
Lachlan J. Rogers <lachlan.j.rogers@quantum.diamonds>
2018-10-20 22:42:38 +1100

    Completed a pack balance log script.

--- scripts/pack_balance_log.py
+++ scripts/pack_balance_log.py
@@ -18,15 +18,13 @@ along with pcm60x_to_gdocs. If not, see <http://www.gnu.org/licenses/>.
 Copyright (c) Lachlan Rogers
 """
 
-import time
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import plotly.plotly as py
 import plotly.graph_objs as go
 from datetime import datetime
 
 PACKNAME = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
 
 def get_input_pack_voltage(packnum):
     """Do user input of pack voltage"""
@@ -37,6 +35,70 @@ def get_input_pack_voltage(packnum):
     return float(pack_v)
 
 
+def add_data_to_chart(measure_time, string_percentages, mean_v):
+    """Add data to the online plotly chart"""
+
+    trace1 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[0]], 
+        name = "pack A", 
+        yaxis = "y2", 
+    )
+    
+    trace2 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[1]], 
+        name = "pack B", 
+        yaxis = "y2", 
+    )
+    
+    trace3 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[2]], 
+        name = "pack C", 
+        yaxis = "y2", 
+    )
+    
+    trace4 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[3]], 
+        name = "pack D", 
+        yaxis = "y2", 
+    )
+    
+    trace5 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[4]], 
+        name = "pack E", 
+        yaxis = "y2", 
+    )
+    
+    trace6 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[5]], 
+        name = "pack F", 
+        yaxis = "y2", 
+    )
+    
+    trace7 = go.Scatter(
+        x = [measure_time], 
+        y = [string_percentages[6]], 
+        name = "pack G", 
+        yaxis = "y2", 
+    )
+    
+    trace8 = go.Scatter(
+        x = [measure_time], 
+        y = [mean_v], 
+        name = "Average pack V", 
+        yaxis = "y", 
+    )
+    
+    newdata = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8]
+    
+    plot_url = py.plot(newdata, filename = 'pack_balance_string1', fileopt='extend')
+
+
 def main():
     """Get pack voltages and update balance plot"""
 
@@ -45,11 +107,28 @@ def main():
     for i in range(7):
         string_voltages[i] = get_input_pack_voltage(i)
 
+    measure_time = datetime.now()
+
     mean_v = np.mean(string_voltages)
 
+    string_percentages = 100 * string_voltages / mean_v - 100
+
+    add_data_to_chart(measure_time, string_percentages, mean_v)
+
+    # Write raw data to text file
+    datestring = str(np.datetime64(measure_time))
+    
+    # generate an array with strings
+    data_arrstr = np.char.mod('%f', string_voltages)
+    # combine to a string
+    data_str = ",".join(data_arrstr)
+
+    with open('string1_pack_v_log.dat', 'a') as fd:
+        fd.write(datestring + ',' + data_str)
+
+    # Print out some feedback upon completion
     top_v = max(string_voltages)
     bottom_v = min(string_voltages)
-
     out_of_balance_range = [100 * bottom_v / mean_v - 100, 100 * top_v / mean_v - 100]
 
     print(string_voltages, out_of_balance_range)
